572e97254b777f17785eb47d707c044370422d92
Negar Sarani <negar.sarani2000@gmail.com>
2023-05-24 11:29:29 +0330

    refetching

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,15 @@ function App() {
     );
     return response.data;
   };
-  const { isLoading, data, error } = useQuery(['posts'], fetchData);
+  const { isLoading, data, error , refetch } = useQuery(['posts'], fetchData, {
+    // cacheTime: 5000,
+    // staleTime: 5000,
+    // refetchOnMount:true,
+    // refetchOnWindowFocus: false,
+    // refetchInterval:2000,
+    // refetchIntervalInBackground:true,
+    enabled:false
+  });
 
   if (isLoading) {
     return <div>loading ...</div>;
@@ -23,7 +31,8 @@ function App() {
 
   return (
     <>
-      {data.map((item) => (
+    <button onClick={refetch}>fetch the data</button>
+      {data?.map((item) => (
         <div key={item.id}>
           {item.id} , {item.username}
         </div>
@@ -33,3 +42,18 @@ function App() {
 }
 
 export default App;
+//cacheTime =
+//توی این کویری کش میتونیم اطلاعات کش شده رو بعد 5 ثانیه پاک کنیم
+/// با پاک شدن کش ، کاربر دوباره لودینگ را مشاهده میکند
+
+//staleTime  =  یعنی اولین فتچ که میخوره به مدت هر زمانی که بهش گفته باشیم صفحه رو ریفرش نگه میداره و بعدش مستقر میشه
+//وقتی میره رو حالت ریفرش  تا همون مدت زمان دیگه هیچ فتچی نمیزنه تا تایمش تموم شه  وقتی تایمش شد اگر خواستی دوباره فتچ میزنه
+
+//حالت دیفالتشم 0 میلی ثانیه است که هر بار فتچ بزنه
+
+//refetchInterval => polling
+// هر چند ثانیه یکبار فتچ میزنه
+// در زمانی که روی این ویندو  فوکوس نباشیم این اتفاق متوفق میشه
+
+// refetchIntervalInBackground =>polling
+// همون اتفاق هر چند بار یکبار قتچ شدن هستش که حتی وقتی روی این ویندو فوکوس نباشیم توی بکگراند داره فتچ میزنه
